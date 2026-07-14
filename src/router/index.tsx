da147d7b8eac60c29fb1b2e0680a9db926d5c78d
Machinery-experience-platform <Tkurbangaliev@mail.ru>
2026-07-15 02:06:30 +0500

    Use HashRouter for GitHub Pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
 import Showroom from "../pages/Showroom/Showroom";
 
 export default function AppRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/showroom" element={<Showroom />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
